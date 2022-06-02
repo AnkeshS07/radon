e3e395a9b63f8c569831f115203276f895e4a6e1
AnkeshS07 <ankeshsharma420@gmail.com>
2022-06-02 15:04:02 +0530

    update

---
+++ src/logger/logger.js
@@ -0,0 +1,5 @@
+const Welcome=function(){
+console.log("Welcome to my application. I am Ankesh sharma and a part of FunctionUp radon cohort.")
+}
+
+module.exports.Welcome=Welcome
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
+const functioncall=require('..logger/logger')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
+functioncall.welcome()
     res.send('My first ever api!')
 });
 
