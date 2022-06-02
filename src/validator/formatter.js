f0748574e1ca2ca4d19ce9a09efcd5a517370bb6
AnkeshS07 <ankeshsharma420@gmail.com>
2022-06-02 23:27:53 +0530

    second assignment

--- src/validator/formatter.js
+++ src/validator/formatter.js
@@ -1,7 +1,8 @@
 const font=function(){
     let text="                  MY NAME IS ANKESH        "
+    let text2="my name is ankesh"
     let lowercase=text.toLowerCase()
-    let uppercase=text.toUpperCase() 
+    let uppercase=text2.toUpperCase() 
     let WhiteSpace=text.trim()  
     console.log(lowercase)
     console.log(uppercase)
